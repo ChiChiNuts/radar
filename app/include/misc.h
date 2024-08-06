24ed42deb4e189423a4ca9ddd7c77600199ea3d8
wormarz <wormarz@outlook.com>
2024-08-07 00:10:12 +0800

    support some basic functions of state machine.

--- app/include/misc.h
+++ app/include/misc.h
@@ -13,6 +13,27 @@ extern "C" {
 
 void delay_ms(unsigned int ms);
 
+/* state machine start */
+#define MAX_SM_NAME_LEN 16
+#define MAX_SM_NUMS     4
+
+struct working_state;
+typedef struct working_state* (*worker)(struct working_state* self, void* args);
+
+struct working_state {
+  char state[MAX_SM_NAME_LEN];
+  worker runner;
+};
+
+#define CHG_STATE(_c, _ns) do { (_c) = stat_lookup((_ns)); } while(0)
+
+void init_state_machine(void);
+int add_state(char *name, worker wkr);
+struct working_state* stat_lookup(char *name);
+
+void state_machine_loop(void);
+/* state machine end */
+
 #ifdef __cplusplus
 }
 #endif
--- app/misc/misc.c
+++ app/misc/misc.c
@@ -1,3 +1,6 @@
+#include "misc.h"
+#include <string.h>
+
 #if (STM32F401xC && USE_HAL_DRIVER)
 #include "stm32f4xx_hal.h"
 
@@ -19,3 +22,43 @@ void delay_us(unsigned int us)
 }
 
 #endif
+
+/* state machine start */
+
+static struct working_state states[MAX_SM_NUMS];
+static struct working_state* cur_stat = &states[0];
+
+void init_state_machine(void)
+{
+    memset(states, 0, sizeof(states));
+}
+
+int add_state(char *name, worker wkr)
+{
+    for(int i = 0; i < MAX_SM_NUMS; ++i) {
+        if (states[i].state[0] == '\0') {
+            strncpy(states[i].state, name, MAX_SM_NAME_LEN);
+            states[i].runner = wkr;
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+void state_machine_loop(void)
+{
+    cur_stat = cur_stat->runner(cur_stat, NULL);
+}
+
+struct working_state* stat_lookup(char *name)
+{
+    for(int i = 0; i < MAX_SM_NUMS; ++i) {
+        if (strncmp(states[i].state, name, MAX_SM_NAME_LEN) == 0) {
+            return &states[i];
+        }
+    }
+
+    return NULL;
+}
+/* state machine end */
