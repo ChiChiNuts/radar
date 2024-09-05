2a82f0ddccd12077e934e5ffcea882a186480ee8
wormarz <wormarz@outlook.com>
2024-09-06 00:33:35 +0800

    add driver files of contactless potentiometer.

---
+++ app/include/as5600.h
@@ -0,0 +1,21 @@
+/*
+ * as5600.h
+ *
+ *  Created on: Sep 6, 2024
+ *      Author: wormarz
+ */
+
+#ifndef INCLUDE_AS5600_H_
+#define INCLUDE_AS5600_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int as5600_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INCLUDE_AS5600_H_ */
