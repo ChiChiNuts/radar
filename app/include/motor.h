b2f4de1377ff0f3ec9fcb70b987fe09f38ae0917
wormarz <wormarz@outlook.com>
2024-10-17 23:50:21 +0800

    add HAL functions(motor/distance/angle).

--- app/include/motor.h
+++ app/include/motor.h
@@ -12,14 +12,12 @@
 extern "C" {
 #endif
 
-int step_motor_init(void);
-void step_motor_stop(void);
-
 void motor_init(void);
 void motor_deinit(void);
 void set_motor_interval(uint16_t ms);
 int step_clockwise(uint32_t n);
 int step_counterclockwise(uint32_t n);
+void step_motor_stop(void);
 
 #ifdef __cplusplus
 }
