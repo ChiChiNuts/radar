2eeba16dc4246dd2d3fad686a68c5b543b4067bf
wormarz <wormarz@outlook.com>
2025-04-18 21:55:33 +0800

    fix: ensure ZEPHYR_BASE is set correctly in top.cmake

--- cmake/top.cmake
+++ cmake/top.cmake
@@ -2,6 +2,7 @@ set(PROJECT_ROOT ${CMAKE_SOURCE_DIR})
 set(KCONFIG_ROOT ${CMAKE_SOURCE_DIR}/Kconfig)
 set(BOARD_DIR ${CMAKE_SOURCE_DIR}/configs)
 set(AUTOCONF_H ${CMAKE_CURRENT_BINARY_DIR}/kconfig/include/generated/autoconf.h)
+set(ZEPHYR_BASE ${CMAKE_SOURCE_DIR})
 
 # Re-configure (Re-execute all CMakeLists.txt code) when autoconf.h changes
 set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${AUTOCONF_H})
